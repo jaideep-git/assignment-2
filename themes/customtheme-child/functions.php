fb28c7b4334da5f3d25987253fe5607a49cd8307
Jai <jaideep@dcmail.ca>
2021-11-01 22:41:23 -0400

    Theme intialization complete

--- themes/customtheme-child/functions.php
+++ themes/customtheme-child/functions.php
@@ -11,9 +11,9 @@
 
 
 function customtheme_enqueue_styles() {
-    wp_enqueue_style( 'customtheme-style', get_stylesheet_uri(),
+    wp_enqueue_style( 'customthemechild-style', get_stylesheet_uri(),
         array( 'twenty-twenty-one-style' ), 
         wp_get_theme()->get('Version') // this only works if you have Version in the style header
     );
 }
-add_action( 'wp_enqueue_scripts', 'customtheme_enqueue_styles' );
+add_action( 'wp_enqueue_scripts', 'customthemechild_enqueue_styles' );
