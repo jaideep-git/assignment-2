6378dbb3cce7593b810064853c80ffc3f4ce7231
Jaideep <sharmajaideep000@gmail.com>
2021-12-07 10:49:55 -0500

    New Plugins

--- themes/customtheme-child/functions.php
+++ themes/customtheme-child/functions.php
@@ -9,6 +9,23 @@
  * @since Twenty Twenty-One 1.0
  */
 
+function showTimeTheme_enqueue_scripts() {
+    $parenthandle = 'twentytwentyone-style'; // This is 'twentytewntyone-style' for the perfect theme.
+    $theme = wp_get_theme();
+    wp_enqueue_style( 
+        $parenthandle, get_template_directory_uri() . '/style.css', 
+        array(),  // if the parent theme code has a dependency, copy it to here
+        $theme->parent()->get('Version')
+    );
+    wp_enqueue_style( 
+        'perfect-style', 
+        get_stylesheet_uri(),
+        array( $parenthandle ),
+        $theme->get('Version') // this only works if you have Version in the style header
+    );
+}
+
+add_action( 'wp_enqueue_scripts', 'showTimeTheme_enqueue_scripts' );
 
 function customthemechild_enqueue_styles() {
     wp_enqueue_style( 'customthemechild-style', get_stylesheet_uri(),
